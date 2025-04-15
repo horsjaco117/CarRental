49036a44865c580f701386db4120339d619a6479
horsjaco117 <jacob_horsley@live.com>
2025-04-14 18:11:56 -0600

    Basic verification stuff was added

--- RentalForm.vb
+++ RentalForm.vb
@@ -8,7 +8,154 @@ Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
-    Private Sub NameTextBox_TextChanged(sender As Object, e As EventArgs) Handles NameTextBox.TextChanged
+
+    Sub SetDefaults()
+        NameTextBox.Text = ""
+        AddressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipCodeTextBox.Text = ""
+        BeginOdometerTextBox.Text = ""
+        EndOdometerTextBox.Text = ""
+        DaysTextBox.Text = ""
+        TotalMilesTextBox.Text = ""
+        MileageChargeTextBox.Text = ""
+        TotalDiscountTextBox.Text = ""
+        TotalChargeTextBox.Text = ""
 
     End Sub
+
+    'Sub ValidateInput()
+    '    Dim nameValid As Boolean = Not String.IsNullOrWhiteSpace(NameTextBox.Text)
+    '    Dim addressValid As Boolean = Not String.IsNullOrWhiteSpace(AddressTextBox.Text)
+    '    Dim cityValid As Boolean = Not String.IsNullOrWhiteSpace(CityTextBox.Text)
+    '    Dim stateValid As Boolean = Not String.IsNullOrWhiteSpace(StateTextBox.Text)
+    '    Dim zipValid As Boolean = Not String.IsNullOrWhiteSpace(ZipCodeTextBox.Text)
+    '    Dim beginOdom As Boolean = Not String.IsNullOrWhiteSpace(BeginOdometerTextBox.Text)
+    '    Dim endOdom As Boolean = Not String.IsNullOrWhiteSpace(EndOdometerTextBox.Text)
+    '    Dim days As Boolean = Not String.IsNullOrWhiteSpace(DaysTextBox.Text)
+    '    Dim totalMi As Boolean = Not String.IsNullOrWhiteSpace(TotalMilesTextBox.Text)
+    '    Dim mileageCharge As Boolean = Not String.IsNullOrWhiteSpace(TotalMilesTextBox.Text)
+    '    Dim discount As Boolean = Not String.IsNullOrWhiteSpace(TotalDiscountTextBox.Text)
+
+    '    Dim allValid As Boolean = nameValid And addressValid And cityValid And stateValid And zipValid And
+    '    beginOdom And endOdom And days And totalMi And mileageCharge And discount
+    'End Sub
+    'Private Sub NameTextBox_TextChanged(sender As Object, e As EventArgs) Handles NameTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub AddressTextBox_TextChanged(sender As Object, e As EventArgs) Handles AddressTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub CityTextBox_TextChanged(sender As Object, e As EventArgs) Handles CityTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub StateTextBox_TextChanged(sender As Object, e As EventArgs) Handles StateTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub ZipCodeTextBox_TextChanged(sender As Object, e As EventArgs) Handles ZipCodeTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub BeginOdometerTextBox_TextChanged(sender As Object, e As EventArgs) Handles BeginOdometerTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub EndOdometerTextBox_TextChanged(sender As Object, e As EventArgs) Handles EndOdometerTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub DaysTextBox_TextChanged(sender As Object, e As EventArgs) Handles DaysTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub TotalMilesTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalMilesTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub MileageChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles MileageChargeTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub DayChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles DayChargeTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub TotalDiscountTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalDiscountTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+
+    'Private Sub TotalChargeTextBox_TextChanged(sender As Object, e As EventArgs) Handles TotalChargeTextBox.TextChanged
+    '    ValidateInput()
+    'End Sub
+    Function ValidInputs() As Boolean
+        Dim valid As Boolean = True
+        Dim message As String
+
+        If NameTextBox.Text = "" Then
+            valid = False
+            NameTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If AddressTextBox.Text = "" Then
+            valid = False
+            AddressTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If CityTextBox.Text = "" Then
+            valid = False
+            CityTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If StateTextBox.Text = "" Then
+            valid = False
+            StateTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If ZipCodeTextBox.Text = "" Then
+            valid = False
+            ZipCodeTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If BeginOdometerTextBox.Text = "" Then
+            valid = False
+            BeginOdometerTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If EndOdometerTextBox.Text = "" Then
+            valid = False
+            EndOdometerTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If DaysTextBox.Text = "" Then
+            valid = False
+            DaysTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If TotalMilesTextBox.Text = "" Then
+            valid = False
+            TotalMilesTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If MileageChargeTextBox.Text = "" Then
+            valid = False
+            MileageChargeTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If TotalDiscountTextBox.Text = "" Then
+            valid = False
+            TotalDiscountTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        If TotalChargeTextBox.Text = "" Then
+            valid = False
+            TotalChargeTextBox.Focus()
+            message &= "Name must be added."
+        End If
+        Return valid
+    End Function
 End Class
