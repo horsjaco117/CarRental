be36b0e52221d8aa38f9c1200ed523003ac5357e
horsjaco117 <jacob_horsley@live.com>
2025-04-20 19:48:02 -0600

    Finalized stuff for the HW.

--- RentalForm.vb
+++ RentalForm.vb
@@ -6,22 +6,10 @@ Option Compare Binary
 'Spring 2025
 'URL: https://github.com/horsjaco117/CarRental
 
-
-
-'[ ] Remove invalid data from textbox 
-'[ ] Set tab order
-'[ ] Validate odometer readings
-'[ ] Validate number of days
-'[ ] single message box to display an improper input
-'[ ] do special mile math for each range
-'[ ] add radio button functions discounts
-'[ ] 
-
 Imports System.Net.Security
 
 Public Class RentalForm
-
-    Private Sub SetDefaults()
+    Private Sub SetDefaults() 'All text boxes user and computed are reset to empty
         NameTextBox.Text = ""
         AddressTextBox.Text = ""
         CityTextBox.Text = ""
@@ -34,47 +22,41 @@ Public Class RentalForm
         MileageChargeTextBox.Text = ""
         TotalDiscountTextBox.Text = ""
         TotalChargeTextBox.Text = ""
-
-
     End Sub
-
-
-    Private Function dailyDollars() As Integer
+    Private Function dailyDollars() As Integer 'Daily charge for car rental is done here
         Dim dailyCharge As Integer
         Dim days As Integer
 
-        If Integer.TryParse(DaysTextBox.Text, days) Then
+        If Integer.TryParse(DaysTextBox.Text, days) Then 'Basic math of $15 per day
             dailyCharge = days * 15
             Return dailyCharge
         Else
             Return 0
         End If
     End Function
-    Private Function test() As Integer
-
+    Private Function accumulatedMileage() As Integer 'This is the math for the total miles taken during rental period
         Dim beginOdometer As Integer
         Dim endOdometer As Integer
         Dim totalMiles As Integer
-        If Integer.TryParse(BeginOdometerTextBox.Text, beginOdometer) AndAlso
+
+        If Integer.TryParse(BeginOdometerTextBox.Text, beginOdometer) AndAlso 'The user input is assigned to the variable
            Integer.TryParse(EndOdometerTextBox.Text, endOdometer) Then
             totalMiles = endOdometer - beginOdometer
-            Return totalMiles
+            Return totalMiles 'Amount of total miles is returned
         Else
             Return 0
         End If
 
     End Function
-    Private Function calculateMiles() As Double
+    Private Function calculateMiles() As Double 'All the math for the miles is included here
         Dim BeginOdometer As Integer
         Dim EndOdometer As Integer
         Dim miles As Double
-        Dim kilometers As Double
         Dim twelveCentMiles As Double
         Dim tenCentMiles As Double
         Dim _mileageCharge As Double
 
-
-        If Integer.TryParse(BeginOdometerTextBox.Text, BeginOdometer) AndAlso
+        If Integer.TryParse(BeginOdometerTextBox.Text, BeginOdometer) AndAlso 'Odometer math was done again to help with math function
             Integer.TryParse(EndOdometerTextBox.Text, EndOdometer) Then
             miles = EndOdometer - BeginOdometer
             'Return miles
@@ -82,7 +64,7 @@ Public Class RentalForm
             Return 0
         End If
 
-        If MilesradioButton.Checked = True Then
+        If MilesradioButton.Checked = True Then 'Math for the miles
 
             Select Case miles
                 Case 0 To 200
@@ -98,7 +80,7 @@ Public Class RentalForm
             End Select
         End If
 
-        If KilometersradioButton.Checked = True Then
+        If KilometersradioButton.Checked = True Then 'Largely similar math, but ties to the kilometers
 
             Select Case miles
                 Case 0 To 200
@@ -114,9 +96,9 @@ Public Class RentalForm
             End Select
         End If
 
-        MileageChargeTextBox.Text = _mileageCharge.ToString("C2")
+        MileageChargeTextBox.Text = _mileageCharge.ToString("C2") 'C2 allows the dollar sign to be added
 
-        Return _mileageCharge
+        Return _mileageCharge 'Charge that would be applied to the card
 
     End Function
 
@@ -127,37 +109,63 @@ Public Class RentalForm
         Dim totalCharge As Double
         Dim discountAmount As Double
 
-        ValidInputs()
+        ValidInputs() 'validation for correct inputs is called for
 
-        totalCharge = dailyDollars() + calculateMiles()
-        If AAAcheckbox.Checked = True Then
+        totalCharge = dailyDollars() + calculateMiles() 'This accumulated the miles and daily charge
+        If AAAcheckbox.Checked = True Then 'AAA discount math
             discount += 0.05
             discountAmount = totalCharge * discount
         End If
-        If Seniorcheckbox.Checked = True Then
+        If Seniorcheckbox.Checked = True Then 'Senior Citizen discount math
             discount += 0.03
             discountAmount = totalCharge * discount
         End If
-        If discount > 0 Then
+        If discount > 0 Then 'No discount math
             discountAmount = totalCharge * discount
             totalCharge -= totalCharge * discount
         End If
 
+        'Conversions to string for the readability
         TotalDiscountTextBox.Text = discountAmount.ToString("C2")
         TotalChargeTextBox.Text = "$" & totalCharge.ToString("C2")
         DayChargeTextBox.Text = dailyDollars.ToString("C2")
         TotalMilesTextBox.Text = MeasureofDistance()
 
-        SummaryButton.Enabled = True
+        SummaryButton.Enabled = True 'Calls for the calculate button to be pressed at least once for a summary
 
-        UserSummary(True)
+        UserSummary(True) 'Called functions and their role
         MileSummary(True, CInt(miles))
         MoneySummary(True, CInt(totalCharge))
     End Sub
 
+    Function UserSummary(counting As Boolean) As Integer 'Logs how many users have input their information into the summary button
+        Static _UserSummary As Integer
+
+        If counting = True Then
+            _UserSummary += 1
+        End If
+
+        Return _UserSummary
+    End Function
+
+    Function MileSummary(counting As Boolean, miles As Integer) As Double 'Logs total mileage put on the vehicle by various users
+        Static _mileSummary As Double
+        If counting = True Then
+            _mileSummary += miles
+        End If
+        Return _mileSummary
+    End Function
+
+    Function MoneySummary(counting As Boolean, charge As Integer) As Double 'Total amount of money made from the car
+        Static _moneySummary As Double
+        If counting = True Then
+            _moneySummary += charge
+        End If
+        Return _moneySummary
+    End Function
 
     Private Sub SummaryButton_Click(sender As Object, e As EventArgs) Handles SummaryButton.Click
-        If ValidInputs() Then
+        If ValidInputs() Then 'When inputs are valid the summary of all the submitted info will be called and shown
             Dim summary As String = ""
 
             summary &= "Number of Customers: " & CStr(UserSummary(False)) & vbNewLine
@@ -167,6 +175,11 @@ Public Class RentalForm
             SetDefaults()
 
         End If
+    End Sub
+
+    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
+        'Disables the summary button at initialization
+        SummaryButton.Enabled = False
     End Sub
 
     Private Function ValidInputs() As Boolean
@@ -251,30 +264,34 @@ Public Class RentalForm
         Return valid
     End Function
 
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-
-        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
-
-        If result = DialogResult.Yes Then
-            Application.Exit()
-        End If
-
-    End Sub
-
-
     Private Sub OnlyLetters(sender As Object, e As KeyPressEventArgs) Handles NameTextBox.KeyPress, CityTextBox.KeyPress, StateTextBox.KeyPress
+        'This limits the user to only putting letters into boxes that call for letters
         If Not Char.IsLetter(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsWhiteSpace(e.KeyChar) Then
             e.Handled = True
         End If
     End Sub
 
     Private Sub OnlyNumbers(sender As Object, e As KeyPressEventArgs) Handles ZipCodeTextBox.KeyPress, BeginOdometerTextBox.KeyPress, EndOdometerTextBox.KeyPress, DaysTextBox.KeyPress
+        'This only allows numbers to be input into certain boxes
         If Not Char.IsDigit(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsControl(e.KeyChar) Then
             e.Handled = True
         End If
     End Sub
 
+    Private Function MeasureofDistance() As String
+        'This is the function that helps with the changing between miles and kilometers.
+        Dim _measureOfDistance As String = " mi."
+        If MilesradioButton.Checked = True Then
+            _measureOfDistance = CStr(accumulatedMileage()) & " mi."
+            Return _measureOfDistance
+        Else
+            _measureOfDistance = CStr(accumulatedMileage()) & " km."
+            Return _measureOfDistance
+        End If
+
+    End Function
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
+        'Specifically for the clear button. Clears all boxes
         NameTextBox.Clear()
         AddressTextBox.Clear()
         CityTextBox.Clear()
@@ -289,53 +306,13 @@ Public Class RentalForm
         TotalDiscountTextBox.Clear()
         TotalChargeTextBox.Clear()
     End Sub
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'Closes the program and politely asks if you hit the button by accident
+        Dim result As DialogResult = MessageBox.Show("Are you sure you want to exit?", "Confirm Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
-    Private Function MeasureofDistance() As String
-
-        Dim _measureOfDistance As String = " mi."
-        If MilesradioButton.Checked = True Then
-            _measureOfDistance = CStr(test()) & " mi."
-            Return _measureOfDistance
-        Else
-            _measureOfDistance = CStr(test()) & " km."
-            Return _measureOfDistance
+        If result = DialogResult.Yes Then
+            Application.Exit()
         End If
-
-    End Function
-
-    Private Sub DaysTextBox_TextChanged(sender As Object, e As EventArgs) Handles DaysTextBox.TextChanged
 
     End Sub
-
-    Private Sub RentalForm_Load(sender As Object, e As EventArgs) Handles Me.Load
-        SummaryButton.Enabled = False
-    End Sub
-
-    Function UserSummary(counting As Boolean) As Integer
-        Static _UserSummary As Integer
-
-        If counting = True Then
-            _UserSummary += 1
-        End If
-
-        Return _UserSummary
-    End Function
-
-    Function MileSummary(counting As Boolean, miles As Integer) As Double
-        Static _mileSummary As Double
-        If counting = True Then
-            _mileSummary += miles
-        End If
-        Return _mileSummary
-    End Function
-    Function MoneySummary(counting As Boolean, charge As Integer) As Double
-        Static _moneySummary As Double
-        If counting = True Then
-            _moneySummary += charge
-        End If
-        Return _moneySummary
-    End Function
-
-
 End Class
-
